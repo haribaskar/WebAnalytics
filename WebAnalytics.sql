755332fda0cacf3d4a77d678707e6f24f071663d
Hari Baskar R <haribaskar98@gmail.com>
2017-02-21 16:13:11 +0530

    new column added in sql file

--- WebAnalytics.sql
+++ WebAnalytics.sql
@@ -1,1 +1,1 @@
-create table request_header(id int not null auto_increment,method varchar(20),domain_name varchar(1000),path varchar(1200),cookie varchar(20),ip varchar(20),user_agent varchar(200),time datetime,primary key(id));
+create table request_header(id int not null auto_increment,method varchar(20),domain_name varchar(1000),path varchar(1200),persistent_cookie varchar(100),session_cookie varchar(100),ip varchar(30),user_agent varchar(200),time datetime,primary key(id));
